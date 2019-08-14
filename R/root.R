e51b04d800365ec3dae200ea4eab1becd69dbd8a
mr-francois <mr.francois@gmx.de>
2019-08-14 23:24:06 +0200

    fixes #1

--- R/root.R
+++ R/root.R
@@ -3,6 +3,6 @@
 #' @param x a vector
 #' @param n a scalar
 #' @export
-root<-function(x,n=6){
+root<-function(x,n=2){
   x^(1/n)
 }
--- tests/testthat/test-1.R
+++ tests/testthat/test-1.R
@@ -1,6 +1,6 @@
 context("root function")
 
-test_that("forced mistake",{
+test_that("test with mistake",{
 expect_equal(root(4,n=2),2)
 expect_equal(root(27,n=3),3)
 expect_equal(root(27,n=3),7)
