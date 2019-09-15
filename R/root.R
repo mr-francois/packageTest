dfcf3b9403105a70438951599634e82bb494b53e
mr-francois <mr.francois@gmx.de>
2019-09-15 17:18:21 +0200

    root_6 new Branch

--- R/root.R
+++ R/root.R
@@ -1,8 +1,8 @@
-#' Computes 5th root of vector
+#' Computes 6th root of vector
 #'
 #' @param x a vector
 #' @param n a scalar
 #' @export
-root<-function(x,n=5){
+root<-function(x,n=6){
   x^(1/n)
 }
