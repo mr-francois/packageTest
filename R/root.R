36832595310a8123afbfa625c5b2e000c0703bb0
mr-francois <mr.francois@gmx.de>
2019-09-15 17:16:24 +0200

    root_5

--- R/root.R
+++ R/root.R
@@ -1,8 +1,8 @@
-#' Computes 4th root of vector
+#' Computes 5th root of vector
 #'
 #' @param x a vector
 #' @param n a scalar
 #' @export
-root<-function(x,n=4){
+root<-function(x,n=5){
   x^(1/n)
 }
